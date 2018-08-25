a4d2f0123eac0fb8c351a9c24178c58ed40eea53
fwl123456 <fwl@lindo.io>
2018-08-25 16:01:40 +0800

    增加文章模糊搜索

--- app/controllers/api/v1/admin/articles_controller.rb
+++ app/controllers/api/v1/admin/articles_controller.rb
@@ -2,7 +2,9 @@ class Api::V1::Admin::ArticlesController < Api::V1::Admin::BaseController
   before_action :set_wx_public, only: [:index, :show]
 
   def index
-    @articles = @wx_public.articles.page(params[:page])
+    @articles = @wx_public.articles
+    @articles = @articles.full_text_search(params[:search]) unless params[:search].blank?
+    @articles = @articles.page(params[:page])
   end
 
   def show
