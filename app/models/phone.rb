71e67dacc08ef0a237c39248224b17951827f2f7
fwl123456 <fwl@lindo.io>
2018-08-31 18:02:02 +0800

    修改phone

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -4,7 +4,7 @@ class Phone
 
   field :sn, type: String # 设备号
   field :name, type: String # 文章名称
-  field :status, type: Integer # 设备状态 -1 掉线 0 未分配 1 使用中
+  field :status, type: Integer, default: 0 # 设备状态 -1 掉线 0 未分配 1 使用中
   # field :online_time, type: Time # 在线时间
   field :start_time, type: Time # 登录时间 
   has_many :article_logs
